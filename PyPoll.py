c2f0b40a20f5151ce1cbe2117eb166fe82dd16e0
Salil V <salilvalia@gmail.com>
2022-09-15 22:06:24 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -9,6 +9,23 @@
 import csv
 import os
 
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join('C:', 'Documents', 'GitHub', 'Election_Analysis', "Resources", "election_results.csv")
+
+# Assign a variable to save file to a path
+file_to_save = os.path.join('C:', 'Documents', 'GitHub', 'Election_Analysis', "analysis", "election_analysis.txt")
+
+#Open the election results and read the file.
+
+with open(file_to_load) as election_data:
+
+    reader = csv.reader(election_data)
+    print(reader)
+
+
+# Print the file object
+print(election_data)
+    
 #Assign a variable to for the file to load and a path
 file_to_load = 'Resources/election_results.csv'
 
